0f422c222e07358e2157bef2e98306e42cfba809
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-09-10 16:25:30 +0200

    posting data to the server

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,9 +14,10 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityDashboard({ activities, selectActivity, deleteActivity,
+export default function ActivityDashboard({ activities, selectActivity, deleteActivity, submitting,
     selectedActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit }: Props) {
     return (
         <Grid>
@@ -35,7 +36,12 @@ export default function ActivityDashboard({ activities, selectActivity, deleteAc
                     openForm={openForm}
                     />}
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                    <ActivityForm
+                    closeForm={closeForm}
+                    activity={selectedActivity}
+                    createOrEdit={createOrEdit}
+                    submitting={submitting }
+                    />}
             </Grid.Column>
         </Grid>
     )
