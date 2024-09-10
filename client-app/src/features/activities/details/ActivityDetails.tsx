09f893341893be32c72c1ed89e1d10bfa730d2bb
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-09-10 18:55:18 +0200

    getting an individual activity

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -3,7 +3,7 @@ import { useStore } from "../../../app/stores/store";
 
 export default function ActivityDetails() {
     const { activityStore } = useStore();
-    const { selectedActivity: activity, openForm, cancelSelectedActivity } = activityStore;    
+    const { selectedActivity: activity } = activityStore;    
 
     if (!activity) return;
 
@@ -21,8 +21,8 @@ export default function ActivityDetails() {
             </CardContent>
             <CardContent extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
-                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel' />
+                    <Button basic color='blue' content='Edit' />
+                    <Button basic color='grey' content='Cancel' />
                 </Button.Group>
             </CardContent>
         </Card>
