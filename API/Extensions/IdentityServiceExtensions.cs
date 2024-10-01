ed5085f33f13d20ba84b0b40ff1f100c95147325
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-10-01 16:45:08 +0200

    Authenticating to the app

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using API.Services;
 using Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Persistence;
+using System.Text;
 
 namespace API.Extensions
 {
@@ -14,7 +17,20 @@ namespace API.Extensions
             })
             .AddEntityFrameworkStores<DataContext>();
 
-            services.AddAuthentication();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("oxpkQyJXVqhDbFv1NVr7of4Wf38YbDRcBCv48gcd6GXgdRjx06EHKh8rLvUNyN94f"));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
             services.AddScoped<TokenService>();
 
             return services;
