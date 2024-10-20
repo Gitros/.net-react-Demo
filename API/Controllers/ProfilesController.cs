6c5b9032608325c2b26668026010708d54393528
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-10-20 18:03:33 +0200

    adding handler for user activities

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,19 +1,20 @@
 ﻿using Application.Profiles;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class ProfilesController : BaseApiController
 {
-    public class ProfilesController : BaseApiController
-    {
-        [HttpGet("{username}")]
-        public async Task<IActionResult> GetProfile(string username)
-        {
-            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
-        }
-        [HttpPut]
-        public async Task<IActionResult> EditProfile(EditProfile.Command command)
-        {
-            return HandleResult(await Mediator.Send(command));
-        }
-    }
+    [HttpGet("{username}")]
+    public async Task<IActionResult> GetProfile(string username) 
+        => HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+
+    [HttpPut]
+    public async Task<IActionResult> EditProfile(EditProfile.Command command) 
+        => HandleResult(await Mediator.Send(command));
+
+    [HttpGet("{username}/activities")]
+    public async Task<IActionResult> GetUserActivities(string username, string predicate) 
+        => HandleResult(await Mediator.Send(new ListActivities.Query 
+            { Username = username, Predicate = predicate}));
 }
